62e00dd8f85bdcf8cecfaa92dbf1aae6ebdeca4a
jzqxx <126733798+jzqxx@users.noreply.github.com>
2026-05-20 14:31:23 +0800

    Add files via upload

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -58,7 +58,7 @@ function clean_packages(){
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
